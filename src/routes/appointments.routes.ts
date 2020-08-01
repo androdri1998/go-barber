075352f7637f284fd34eb5e761b161048f06305c
André Rodrigues <andre.rodrigues98@outlook.com>
2020-08-01 00:09:14 -0300

    inserindo base da aplicação

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRepository = new AppointmentsRepository();
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.post('/', (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointmentService = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointmentService.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return res.json({ ...appointment });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+  return res.json(appointments);
+});
+
+export default appointmentsRouter;
