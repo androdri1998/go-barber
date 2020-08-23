61a0abbce2828b0ea026faf224fb26c1218d965c
André Rodrigues <andre.rodrigues98@outlook.com>
2020-08-23 12:09:40 -0300

    inserindo estrutura de testes

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,41 @@
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const mockAppointment = {
+      date: new Date(2020, 10, 10, 11),
+      provider_id: '123456',
+    };
+
+    const appointment = await createAppointment.execute(mockAppointment);
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe(mockAppointment.provider_id);
+  });
+
+  it('should not be able to create a new appointment on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const mockAppointment = {
+      date: new Date(),
+      provider_id: '123456',
+    };
+
+    await createAppointment.execute(mockAppointment);
+
+    expect(createAppointment.execute(mockAppointment)).rejects.toBeInstanceOf(
+      AppError,
+    );
+  });
+});
